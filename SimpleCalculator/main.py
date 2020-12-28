38c31197496c918ab1c8778115ad82b28fd902e7
A-gamedev-noob <56467973+A-gamedev-noob@users.noreply.github.com>
2020-12-28 11:52:46 +0530

    project created

---
+++ SimpleCalculator/main.py
@@ -0,0 +1,13 @@
+import tkinter
+
+_list = []
+
+root = tkinter.Tk()
+
+label = tkinter.Label(root, text="This is the label")
+label2 = tkinter.Label(root, text="2nd label")
+
+label.pack()
+label2.pack()
+
+root.mainloop()
