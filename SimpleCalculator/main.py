e206188dd5ee1af3294f9daf3290813d87ad7dbd
A-gamedev-noob <56467973+A-gamedev-noob@users.noreply.github.com>
2020-12-28 12:03:54 +0530

    tweaks

--- SimpleCalculator/main.py
+++ SimpleCalculator/main.py
@@ -6,8 +6,10 @@ root = tkinter.Tk()
 
 label = tkinter.Label(root, text="This is the label")
 label2 = tkinter.Label(root, text="2nd label")
+label3 = tkinter.Label(root, text="3rd label")
 
 label.pack()
 label2.pack()
+label3.pack()
 
 root.mainloop()
